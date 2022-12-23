dd951c67d99105af3dc83c7b10180195a8272381
camillaPodda <camillapodda.2000@gmail.com>
2022-12-23 15:54:53 +0100

    Add Theme

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -4,7 +4,9 @@ export const theme = {
     secondary: '#78A9FF',
     secondaryDark: '#285AB3',
     text: '#28282C',
-    textInverse: 'F0F0F0'
+    textInverse: '#F0F0F0',
+    background: '',
+    backgroundDark: ''
   },
   fontSizes: {
     // rem
@@ -13,7 +15,8 @@ export const theme = {
     md: 1,
     lg: 1.6,
     xl: 2.2
-  }
+  },
+  radii: [4, 8, 12]
 }
 
 export type AppTheme = typeof theme
