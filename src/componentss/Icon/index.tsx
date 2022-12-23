8e0e0671eb5e8acb67b91e0e925bfa03b734b84a
camillaPodda <camillapodda.2000@gmail.com>
2022-12-23 16:28:18 +0100

    Add Icon

--- src/componentss/Icon/index.tsx
+++ src/componentss/Icon/index.tsx
@@ -1,1 +1,37 @@
-export {}
+import { SizeProp } from '@fortawesome/fontawesome-svg-core'
+import {
+  faArrowLeft,
+  faArrowRight,
+  faBagShopping,
+  faStar
+} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { theme, ThemeColor } from '../../style/theme'
+
+type IconName = keyof typeof iconsMap
+
+type Props = {
+  name: IconName
+  size?: SizeProp
+  color?: ThemeColor
+  bgColor?: ThemeColor
+  className?: string
+}
+
+const iconsMap = {
+  bagShopping: faBagShopping,
+  arrowRight: faArrowRight,
+  arrowLeft: faArrowLeft,
+  star: faStar
+}
+
+export function Icon({ size, name, className, color = 'background' }: Props) {
+  return (
+    <FontAwesomeIcon
+      className={className}
+      icon={iconsMap[name]}
+      color={theme.colors[color]}
+      size={size}
+    />
+  )
+}
