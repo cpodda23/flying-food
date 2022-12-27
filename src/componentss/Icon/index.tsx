48d023163886f7bb561c27af1082d6b7e34f3ea8
camillaPodda <camillapodda.2000@gmail.com>
2022-12-27 23:05:30 +0100

    Add icon names

--- src/componentss/Icon/index.tsx
+++ src/componentss/Icon/index.tsx
@@ -1,9 +1,36 @@
 import { SizeProp } from '@fortawesome/fontawesome-svg-core'
 import {
+  faTwitter,
+  faInstagram,
+  faGooglePlus,
+  faFacebook,
+  faLinkedinIn
+} from '@fortawesome/free-brands-svg-icons'
+import {
+  faAppleWhole,
   faArrowLeft,
   faArrowRight,
+  faAward,
   faBagShopping,
-  faStar
+  faBowlFood,
+  faBurger,
+  faCake,
+  faCarrot,
+  faFish,
+  faGift,
+  faHeart,
+  faIceCream,
+  faLocationDot,
+  faMagnifyingGlass,
+  faMartiniGlassCitrus,
+  faMinus,
+  faPhone,
+  faPizzaSlice,
+  faPlus,
+  faSeedling,
+  faStar,
+  faTruck,
+  faUser
 } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { theme, ThemeColor } from '../../style/theme'
@@ -22,7 +49,32 @@ const iconsMap = {
   bagShopping: faBagShopping,
   arrowRight: faArrowRight,
   arrowLeft: faArrowLeft,
-  star: faStar
+  star: faStar,
+  phone: faPhone,
+  burger: faBurger,
+  pizza: faPizzaSlice,
+  beverages: faMartiniGlassCitrus,
+  iceCream: faIceCream,
+  vegetables: faCarrot,
+  fruit: faAppleWhole,
+  dessert: faCake,
+  sushi: faFish,
+  soups: faBowlFood,
+  salad: faSeedling,
+  personal: faUser,
+  map: faLocationDot,
+  delivery: faTruck,
+  award: faAward,
+  bonuses: faGift,
+  heart: faHeart,
+  search: faMagnifyingGlass,
+  plus: faPlus,
+  minus: faMinus,
+  twitter: faTwitter,
+  instagram: faInstagram,
+  googlePlus: faGooglePlus,
+  facebook: faFacebook,
+  linkedin: faLinkedinIn
 }
 
 export function Icon({ size, name, className, color = 'background' }: Props) {
