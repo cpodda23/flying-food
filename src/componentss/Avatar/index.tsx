5b8cf4c03086d7267e23c964220459d6e5393508
camillaPodda <camillapodda.2000@gmail.com>
2022-12-27 11:57:39 +0100

    Refactor Avatar

--- src/componentss/Avatar/index.tsx
+++ src/componentss/Avatar/index.tsx
@@ -1,36 +1,46 @@
 import { ThemeColor } from '../../style/theme'
+import { Image } from '../Image'
 import { Text } from '../Text'
 import { StyledAvatar } from './styled'
 
 type Props = {
   username?: string
   imageSrc?: string
+  size?: number
   color?: ThemeColor
   bgColor?: ThemeColor
   className?: string
+}
+
+const getInitials = (username: string) => {
+  const initials = username.split(' ')
+  const first = initials.at(0)?.charAt(0) || ''
+  const last = initials.at(-1)?.charAt(0) || ''
+  return `${first}${last}`
 }
 
 export const Avatar = ({
   className,
   username,
   imageSrc,
+  size = 64,
   bgColor = 'secondary',
   color = 'textInverse'
 }: Props) => {
-  const getInitials = () => {
-    if (!username) return
-    const initials = username.split(' ')
-    const first = initials.at(0)?.charAt(0) || ''
-    const last = initials.at(-1)?.charAt(0) || ''
-    return `${first}${last}`
+  const renderContent = () => {
+    if (imageSrc) return <Image width="100%" height="100%" fit="cover" src={imageSrc} />
+    if (username)
+      return (
+        <Text size="lg" color={color} upperCase>
+          {getInitials(username)}
+        </Text>
+      )
+    return null
   }
 
   return (
-    <StyledAvatar className={className} bgColor={bgColor}>
-      <Text color={color} upperCase>
-        {getInitials()}
-      </Text>
-      <img src={image} />
+    <StyledAvatar className={className} bgColor={bgColor} size={size}>
+      {renderContent()}
     </StyledAvatar>
   )
 }
--- src/componentss/Avatar/styled.ts
+++ src/componentss/Avatar/styled.ts
@@ -1,8 +1,15 @@
 import styled from '@emotion/styled'
 import { ThemeColor } from '../../style/theme'
+import { center } from '../../style/utils'
 
 export const StyledAvatar = styled.div<{
   bgColor: ThemeColor
+  size: number
 }>`
   background-color: ${({ theme, bgColor }) => theme.colors[bgColor]};
+  width: ${({ size }) => size}px;
+  height: ${({ size }) => size}px;
+  border-radius: 50%;
+  overflow: hidden;
+  ${center}
 `
