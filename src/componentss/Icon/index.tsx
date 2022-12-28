eccdf20d186ac3ee697365996607e5fe42351373
camillaPodda <camillapodda.2000@gmail.com>
2022-12-28 10:22:52 +0100

    Extract Icons map to separate file

--- src/componentss/Icon/index.tsx
+++ src/componentss/Icon/index.tsx
@@ -1,41 +1,7 @@
 import { SizeProp } from '@fortawesome/fontawesome-svg-core'
-import {
-  faTwitter,
-  faInstagram,
-  faGooglePlus,
-  faFacebook,
-  faLinkedinIn
-} from '@fortawesome/free-brands-svg-icons'
-import {
-  faAppleWhole,
-  faArrowLeft,
-  faArrowRight,
-  faAward,
-  faBagShopping,
-  faBowlFood,
-  faBurger,
-  faCake,
-  faCarrot,
-  faFish,
-  faGift,
-  faHeart,
-  faIceCream,
-  faLocationDot,
-  faMagnifyingGlass,
-  faMartiniGlassCitrus,
-  faMinus,
-  faPhone,
-  faPizzaSlice,
-  faPlus,
-  faSeedling,
-  faStar,
-  faTruck,
-  faUser
-} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { theme, ThemeColor } from '../../style/theme'
-
-export type IconName = keyof typeof iconsMap
+import { IconName, iconsMap } from './config'
 
 type Props = {
   name: IconName
@@ -43,38 +9,6 @@ type Props = {
   color?: ThemeColor
   bgColor?: ThemeColor
   className?: string
-}
-
-const iconsMap = {
-  bagShopping: faBagShopping,
-  arrowRight: faArrowRight,
-  arrowLeft: faArrowLeft,
-  star: faStar,
-  phone: faPhone,
-  burger: faBurger,
-  pizza: faPizzaSlice,
-  beverages: faMartiniGlassCitrus,
-  iceCream: faIceCream,
-  vegetables: faCarrot,
-  fruit: faAppleWhole,
-  dessert: faCake,
-  sushi: faFish,
-  soups: faBowlFood,
-  salad: faSeedling,
-  personal: faUser,
-  map: faLocationDot,
-  delivery: faTruck,
-  award: faAward,
-  bonuses: faGift,
-  heart: faHeart,
-  search: faMagnifyingGlass,
-  plus: faPlus,
-  minus: faMinus,
-  twitter: faTwitter,
-  instagram: faInstagram,
-  googlePlus: faGooglePlus,
-  facebook: faFacebook,
-  linkedin: faLinkedinIn
 }
 
 export function Icon({ size, name, className, color = 'background' }: Props) {
