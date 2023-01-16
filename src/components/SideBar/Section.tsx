1dd1f8f64b88f2fef013089f6fa6885da35e6634
camillaPodda <camillapodda.2000@gmail.com>
2023-01-16 12:52:36 +0100

    Add Stack component to SideBar

--- src/components/SideBar/Section.tsx
+++ src/components/SideBar/Section.tsx
@@ -1,5 +1,6 @@
 import { IconName } from '../Icon'
 import { MenuLink } from '../MenuLink'
+import { Stack } from '../Stack'
 import { Text } from '../Text'
 
 type Props = {
@@ -10,22 +11,26 @@ type Props = {
 export const Section = ({ title, items }: Props) => {
   return (
     <div className="section">
-      <Text upperCase bold variant="p" color="textInverse">
-        {title}
-      </Text>
-      <div>
-        {items.map((item) => {
-          return (
-            <MenuLink
-              title={item.name}
-              icon={item.icon}
-              linkUrl=""
-              color="textInverse"
-              key={item.name}
-            />
-          )
-        })}
-      </div>
+      <Stack direction="vertical" gap={12}>
+        <Text upperCase bold variant="p" color="textInverse">
+          {title}
+        </Text>
+        <div>
+          <Stack direction="vertical" gap={12}>
+            {items.map((item) => {
+              return (
+                <MenuLink
+                  title={item.name}
+                  icon={item.icon}
+                  linkUrl=""
+                  color="textInverse"
+                  key={item.name}
+                />
+              )
+            })}
+          </Stack>
+        </div>
+      </Stack>
     </div>
   )
 }
--- src/components/SideBar/styled.ts
+++ src/components/SideBar/styled.ts
@@ -4,11 +4,15 @@ export const StyledSideBar = styled.div`
   background-color: ${({ theme }) => theme.colors.backgroundDark};
   border-top-right-radius: ${({ theme }) => theme.radii[3]}px;
   border-bottom-right-radius: ${({ theme }) => theme.radii[3]}px;
-  width: 20%;
+  width: max-content;
   padding: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 32px;
 
   .section {
-    gap: 20px;
     border-bottom: 1px solid ${({ theme }) => theme.colors.lightGrey};
+    padding-bottom: ${({ theme }) => theme.spacings.lg}px;
   }
 `
