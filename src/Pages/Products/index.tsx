724a13b1a37fa59cd6d25e6e9bd23af259e1595d
camillaPodda <camillapodda.2000@gmail.com>
2023-02-02 18:00:41 +0100

    Get Products from server

--- src/Pages/Products/index.tsx
+++ src/Pages/Products/index.tsx
@@ -1,3 +1,26 @@
+import { useEffect, useState } from 'react'
+import { getProducts } from '../../api'
+import { ProductDto } from '../../api/types'
+import { ProductCard } from '../../components/ProductCard'
+
 export const Products = () => {
-  return <div>Products</div>
+  const [products, setProducts] = useState<ProductDto[]>([])
+
+  useEffect(() => {
+    getProducts().then(setProducts)
+  }, [])
+
+  return (
+    <div>
+      {products.map((product) => (
+        <ProductCard
+          name={product.name}
+          imgSrc={product.imageUrl}
+          rating={product.rating}
+          // price={product.price}
+          //  size={product.size}
+        />
+      ))}
+    </div>
+  )
 }
