88e0c9a20927f9e3f36e80943fc22bbbd00d66af
camillaPodda <camillapodda.2000@gmail.com>
2023-02-05 16:08:37 +0100

    minor refactor Producs style

--- src/Pages/Products/styled.ts
+++ src/Pages/Products/styled.ts
@@ -6,6 +6,7 @@ export const StyledProducts = styled.div`
 `
 export const StyledGrid = styled.div`
   display: grid;
+  max-width: ${({ theme }) => theme.containers.content}px;
   grid-template-columns: repeat(4, 1fr);
   gap: 60px;
 `
