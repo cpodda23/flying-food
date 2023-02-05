d5d7d5dcf1355476fed41a4b8879db4ea7ce528a
camillaPodda <camillapodda.2000@gmail.com>
2023-02-05 15:51:20 +0100

    Add Loading to Products

--- src/Pages/Products/index.tsx
+++ src/Pages/Products/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { getProducts, getTags } from '../../api'
 import { ProductDto, TagDto } from '../../api/types'
 import { Button } from '../../components/Button/Button'
+import { Loading } from '../../components/Loading'
 import { ProductCard } from '../../components/ProductCard'
 import { SectionTitle } from '../../components/SectionTitle'
 import { Stack } from '../../components/Stack'
@@ -11,9 +12,9 @@ export const Products = () => {
   const [products, setProducts] = useState<ProductDto[]>()
   const [tags, setTags] = useState<TagDto[]>()
   const [tagFilter, setTagFilter] = useState('')
-  // const [loading, setLoading] = useState(false)
-  // creare component Loading già pronto su https://loading.io/css/
-  /*   useEffect(() => {
+  /* const [loading, setLoading] = useState(false)
+  
+    useEffect(() => {
     setLoading(true)
     Promise.all([getProducts(), getTags()])
       .then(([p, t]) => {
@@ -23,49 +24,50 @@ export const Products = () => {
       .then(() => setLoading(false))
   }, []) */
 
-  // console.log(loading)
-
   useEffect(() => {
     getProducts().then(setProducts)
     getTags().then(setTags)
-  })
+  }, [])
 
   const filteredProducts = tagFilter
-    ? products.filter((product) => product.tags.some((tag) => tag === tagFilter))
+    ? products?.filter((product) => product.tags.some((tag) => tag === tagFilter))
     : products
 
   return (
-    // (products && tags) gestire loading senza stato loading
-    <StyledProducts>
-      <Stack gap={50}>
-        <SectionTitle main="our" secondary="production" />
-        <Stack>
-          {tags?.map((tag) => (
-            <Button
-              key={tag.id}
-              bgColor="textInverse"
-              onClick={() => setTagFilter(tag.id)}
-            >
-              {tag.name}
-            </Button>
-          ))}
+    //  gestire loading senza stato loading
+    products && tags ? (
+      <StyledProducts>
+        <Stack gap={50}>
+          <SectionTitle main="our" secondary="production" />
+          <Stack>
+            {tags.map((tag) => (
+              <Button
+                key={tag.id}
+                bgColor="textInverse"
+                onClick={() => setTagFilter(tag.id)}
+              >
+                {tag.name}
+              </Button>
+            ))}
+          </Stack>
+          <Button outlined bgColor="background">
+            go to catalog
+          </Button>
         </Stack>
-        <Button outlined bgColor="background">
-          go to catalog
-        </Button>
-      </Stack>
-      <StyledGrid>
-        {filteredProducts.map((product) => (
-          <ProductCard
-            key={product.id}
-            // size={product.size.type}
-            name={product.name}
-            imgSrc={product.imageUrl}
-            rating={product.rating}
-            price={product.price}
-          />
-        ))}
-      </StyledGrid>
-    </StyledProducts>
+        <StyledGrid>
+          {filteredProducts?.map((product) => (
+            <ProductCard
+              key={product.id}
+              name={product.name}
+              imgSrc={product.imageUrl}
+              rating={product.rating}
+              price={product.price}
+            />
+          ))}
+        </StyledGrid>
+      </StyledProducts>
+    ) : (
+      <Loading />
+    )
   )
 }
