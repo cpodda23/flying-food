dd76f4f24aa10dcf1edf31a9463c61e153006cce
camillaPodda <camillapodda.2000@gmail.com>
2023-02-06 18:59:45 +0100

    Refactor Products page

--- src/Pages/Products/index.tsx
+++ src/Pages/Products/index.tsx
@@ -1,11 +1,9 @@
 import { useEffect, useState } from 'react'
 import { getProducts, getTags } from '../../api'
 import { ProductDto, TagDto } from '../../api/types'
-import { Button } from '../../components/Button/Button'
 import { Loading } from '../../components/Loading'
 import { ProductCard } from '../../components/ProductCard'
-import { SectionTitle } from '../../components/SectionTitle'
-import { Stack } from '../../components/Stack'
+import { PageHeader } from './PageHeader'
 import { StyledGrid, StyledProducts } from './styled'
 
 export const Products = () => {
@@ -25,49 +23,41 @@ export const Products = () => {
   }, []) */
 
   useEffect(() => {
-    getProducts().then(setProducts)
-    getTags().then(setTags)
+    Promise.all([getProducts(), getTags()]).then(([p, t]) => {
+      setTagFilter(t.find(({ hidden }) => !hidden)?.id || '')
+      setProducts(p)
+      setTags(t)
+    })
   }, [])
 
+  if (!products || !tags) {
+    return (
+      <div>
+        <Loading />
+      </div>
+    )
+  }
+
+  const filteredTags = tags.filter((tag) => !tag.hidden)
+
   const filteredProducts = tagFilter
-    ? products?.filter((product) => product.tags.some((tag) => tag === tagFilter))
+    ? products.filter((product) => product.tags.some((tag) => tag === tagFilter))
     : products
 
   return (
-    //  gestire loading senza stato loading
-    products && tags ? (
-      <StyledProducts>
-        <Stack gap={50}>
-          <SectionTitle main="our" secondary="production" />
-          <Stack>
-            {tags.map((tag) => (
-              <Button
-                key={tag.id}
-                bgColor="textInverse"
-                onClick={() => setTagFilter(tag.id)}
-              >
-                {tag.name}
-              </Button>
-            ))}
-          </Stack>
-          <Button outlined bgColor="background">
-            go to catalog
-          </Button>
-        </Stack>
-        <StyledGrid>
-          {filteredProducts?.map((product) => (
-            <ProductCard
-              key={product.id}
-              name={product.name}
-              imgSrc={product.imageUrl}
-              rating={product.rating}
-              price={product.price}
-            />
-          ))}
-        </StyledGrid>
-      </StyledProducts>
-    ) : (
-      <Loading />
-    )
+    <StyledProducts>
+      <PageHeader tags={filteredTags} selectedTag={tagFilter} onTagClick={setTagFilter} />
+      <StyledGrid>
+        {filteredProducts?.map((product) => (
+          <ProductCard
+            key={product.id}
+            name={product.name}
+            imgSrc={product.imageUrl}
+            rating={product.rating}
+            price={`${product.price.type} ${product.price.value}`}
+          />
+        ))}
+      </StyledGrid>
+    </StyledProducts>
   )
 }
