e3de99b8917005a18a8902a0b29a3e11767e52ed
camillaPodda <camillapodda.2000@gmail.com>
2023-02-06 18:45:37 +0100

    Add PageHeader to Products page

---
+++ src/Pages/Products/PageHeader.tsx
@@ -0,0 +1,35 @@
+import { TagDto } from '../../api/types'
+import { Button } from '../../components/Button'
+import { SectionTitle } from '../../components/SectionTitle'
+import { Stack } from '../../components/Stack'
+
+type Props = {
+  tags: TagDto[]
+  onTagClick: (id: string) => void
+  selectedTag?: string
+}
+
+export const PageHeader = ({ tags, onTagClick, selectedTag }: Props) => {
+  return (
+    <Stack gap={50}>
+      <SectionTitle main="our" secondary="production" />
+      <Stack>
+        {tags.map((tag) => (
+          <Button
+            inverse={selectedTag === tag.id}
+            key={tag.id}
+            bgColor="textInverse"
+            onClick={() => {
+              onTagClick(tag.id)
+            }}
+          >
+            {tag.name}
+          </Button>
+        ))}
+      </Stack>
+      <Button outlined bgColor="background">
+        go to catalog
+      </Button>
+    </Stack>
+  )
+}
