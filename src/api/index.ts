c23e73e6bafc35f7cf9e56f4176295b4c597b4d0
camillaPodda <camillapodda.2000@gmail.com>
2023-02-06 19:31:09 +0100

    Add get functions

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,7 +5,14 @@ export const getProducts = () => apiClient.get<ProductDto[]>('products')
 
 export const getTags = () => apiClient.get<TagDto[]>('tags')
 
+export const getProductsById = (id: string) => apiClient.get<ProductDto>(`products/${id}`)
+
+export const getRandomProducts = () => apiClient.get<ProductDto[]>('products/random')
+
 export const getDeliveries = () => apiClient.get<DeliveryDto>('deliveries')
 
 export const createOrder = (body: OrderDto) =>
   apiClient.post<CreateOrderRes>('order', body)
+
+// getproducts by id
+// get products random
