30f719189204d30f9354b380d5c1ca8973675b84
camillaPodda <camillapodda.2000@gmail.com>
2023-03-13 17:29:10 +0100

    Provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Global } from '@emotion/react'
 import { Provider } from 'react-redux'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { Layout } from './components/Layout'
-import { store } from './features/store'
 import { tagsActions } from './features/tags/reducer'
 import { useAction } from './hooks/useAction'
 import { Cart } from './Pages/Cart'
@@ -14,25 +13,23 @@ import { globalStyle } from './style/global'
 import { theme } from './style/theme'
 
 function App() {
-  //  useAction(tagsActions.fetchTags(), [])
+  useAction(tagsActions.fetchTags(), [])
 
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <Global styles={globalStyle} />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Navigate to="products" />} />
-            <Route path="/" element={<Layout />}>
-              <Route path="products" element={<Products />} />
-              <Route path="products/:id" element={<ProductDetail />} />
-              <Route path="cart" element={<Cart />} />
-              <Route path="*" element={<NotFound />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </ThemeProvider>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Global styles={globalStyle} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="products" />} />
+          <Route path="/" element={<Layout />}>
+            <Route path="products" element={<Products />} />
+            <Route path="products/:id" element={<ProductDetail />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
